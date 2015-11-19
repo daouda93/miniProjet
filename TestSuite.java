ab70b7746f3f0c90bf7eea39d4e53f10a5975cbf
daouda93 <diopd93@gmail.com>
2015-11-19 11:52:38 +0100

    ajout TestSuite

---
+++ TestSuite.java
@@ -0,0 +1,14 @@
+package tab;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		TestTabtrie.class,
+		TestRecherche.class
+	})
+	public class TestSuite {
+
+	}
+
